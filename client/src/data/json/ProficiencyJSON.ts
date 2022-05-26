b96a35675318de721aef952a0fe6b3cea8fb3036
Luca Fernandez <luca.fernandez@spot.it>
2022-05-26 12:05:52 +0200

    updated proficiency

--- client/src/data/json/ProficiencyJSON.ts
+++ client/src/data/json/ProficiencyJSON.ts
@@ -5,5 +5,18 @@ export default [
   { id: 4, proficiency: 2 },
   { id: 5, proficiency: 3 },
   { id: 6, proficiency: 3 },
-  { id: 7, proficiency: 3 }
+  { id: 7, proficiency: 3 },
+  { id: 8, proficiency: 3 },
+  { id: 9, proficiency: 4 },
+  { id: 10, proficiency: 4 },
+  { id: 11, proficiency: 4 },
+  { id: 12, proficiency: 4 },
+  { id: 13, proficiency: 5 },
+  { id: 14, proficiency: 5 },
+  { id: 15, proficiency: 5 },
+  { id: 16, proficiency: 5 },
+  { id: 17, proficiency: 6 },
+  { id: 18, proficiency: 6 },
+  { id: 19, proficiency: 6 },
+  { id: 20, proficiency: 6 }
 ]
